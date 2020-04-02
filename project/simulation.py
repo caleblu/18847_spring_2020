b451981152fae143ebbab940cf77bea9c5e26dfe
caleblu <kaijil@andrew.cmu.edu>
2020-04-01 20:28:12 -0700

    added encoding

--- project/simulation.py
+++ project/simulation.py
@@ -1,5 +1,7 @@
 import itertools
 import os
+from itertools import combinations_with_replacement, combinations, permutations
+import numpy as np
 
 
 class Dictionary(object):
@@ -17,6 +19,44 @@ class Dictionary(object):
     def __len__(self):
         return len(self.idx2word)
 
+    def get_encoding(self, length, spike):
+        comb = [
+            list(permutations(p)) for p in list(
+                combinations_with_replacement(np.arange(spike), length))
+        ]
+        self.idx2spike = np.array(
+            list(set([item for sublist in comb for item in sublist
+                     ]))[:len(self.word2idx)])
+
+
+class SpikeData(object):
+
+    def __init__(self, tokens, sentences, corpus):
+        self.sentences = sentences
+        self.tokens = tokens
+        self.corpus = corpus
+        self.length = len(self.tokens[0])
+        self.data_size = len(self.tokens)
+
+    def convert_tokens(self, window_size):
+        spike_input = []
+        spike_output = []
+        ws = 2 * window_size + 1
+        for t in self.tokens:
+            for i in range(self.length - ws + 1):
+                spike_input.append(
+                    self.corpus.dictionary.idx2spike.take(np.hstack(
+                        (t[i:window_size + i],
+                         t[window_size + 1 + i:window_size + 1 + i +
+                           window_size])),
+                                                          axis=0))
+                spike_output.append(
+                    self.corpus.dictionary.idx2spike.take(t[window_size + i],
+                                                          axis=0))
+        spike_input = np.array(spike_input)
+        spike_output = np.array(spike_output)
+        return spike_input, spike_output
+
 
 class Corpus(object):
 
@@ -26,26 +66,28 @@ class Corpus(object):
     def tokenize(self, sentences):
 
         for line in sentences:
-            words = line.split() + ['<eos>']
+            words = line.split()
             for word in words:
                 self.dictionary.add_word(word)
 
         # Tokenize file content
         idss = []
         for line in sentences:
-            words = line.split() + ['<eos>']
+            words = line.split()
             ids = []
             for word in words:
                 ids.append(self.dictionary.word2idx[word])
             idss.append(ids)
 
-        return idss
+        return np.array(idss)
 
 
 class Simulation():
 
     def __init__(self):
         self.terminals = {
+            'S1': ['<sos>'],
+            'S2': ['<sos>'],
             'D': ['the'],
             'SUBJ': [
                 'cat', 'dog', 'cow', 'sheep', 'goat', 'pig', 'duck', 'chicken',
@@ -61,7 +103,9 @@ class Simulation():
                 'table', 'chair', 'sofa', 'mat', 'tv', 'dresser', 'shelf',
                 'couch', 'desk', 'lamp', 'computer', 'keyboards'
             ],
-            'EOS': ['.']
+            'PERIOD': ['.'],
+            'E1': ['<eos>'],
+            'E2': ['<eos>']
         }
 
     def construct_sentences(self):
